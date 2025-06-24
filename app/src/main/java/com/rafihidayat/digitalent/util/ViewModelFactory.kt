5a5398c481ca02ed02f7754cb1504741cb06079d
Rafi Hidayat <38679875+Virtue1040@users.noreply.github.com>
2025-06-24 11:59:46 +0700

    Mengganti mainviewmodel menjadi loginviewmodel

--- app/src/main/java/com/rafihidayat/digitalent/util/ViewModelFactory.kt
+++ app/src/main/java/com/rafihidayat/digitalent/util/ViewModelFactory.kt
@@ -3,13 +3,16 @@ package com.rafihidayat.digitalent.util
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.rafihidayat.digitalent.database.UserDao
-import com.rafihidayat.digitalent.ui.screen.MainViewModel
+import com.rafihidayat.digitalent.ui.viewmodel.LoginViewModel
 
-class ViewModelFactory(private val dao: UserDao) : ViewModelProvider.Factory {
-    @Suppress("unchecked_cast")
+class ViewModelFactory(
+    private val dao: UserDao
+
+    ) : ViewModelProvider.Factory {
+        @Suppress("unchecked_cast")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            return MainViewModel(dao) as T
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(dao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
