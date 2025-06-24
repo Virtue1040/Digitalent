2c61575524704b158c668884491797b8e2f5c21e
Rafi Hidayat <38679875+Virtue1040@users.noreply.github.com>
2025-06-24 11:58:43 +0700

    Menambahkan login dan regiter screen, dengan menyesuaikan input dan buttonnya

--- app/src/main/java/com/rafihidayat/digitalent/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/rafihidayat/digitalent/ui/screen/LoginScreen.kt
@@ -1,18 +1,41 @@
 package com.rafihidayat.digitalent.ui.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.rafihidayat.digitalent.R
+import com.rafihidayat.digitalent.ui.theme.DigitalentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,10 +46,9 @@ fun LoginScreen(navController: NavHostController) {
                 title = {
                     Text(
                         text = stringResource(
-                            id = R.string.app_name
+                            id = R.string.app_name_login
                         )
                     )
-
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -41,5 +63,81 @@ fun LoginScreen(navController: NavHostController) {
 
 @Composable
 private fun ScreenContent(modifier: Modifier, navController: NavController) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(60.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            OutlinedTextField(
+                value = username,
+                onValueChange = {
+                    username = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.username)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.password)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(
+                modifier = Modifier.padding(10.dp)
+            )
+
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth().background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(100.dp)
+                )
+                ,
+                border = BorderStroke(0.dp, Color.Transparent),
+                onClick = {
+
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.login),
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LoginScreenPreview() {
+    DigitalentTheme {
+        RegisterScreen(rememberNavController())
+    }
 }
--- app/src/main/java/com/rafihidayat/digitalent/ui/screen/RegisterScreen.kt
+++ app/src/main/java/com/rafihidayat/digitalent/ui/screen/RegisterScreen.kt
@@ -1,18 +1,43 @@
 package com.rafihidayat.digitalent.ui.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.rafihidayat.digitalent.R
+import com.rafihidayat.digitalent.ui.theme.DigitalentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,10 +48,9 @@ fun RegisterScreen(navController: NavHostController) {
                 title = {
                     Text(
                         text = stringResource(
-                            id = R.string.app_name
+                            id = R.string.app_name_register
                         )
                     )
-
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -41,5 +65,122 @@ fun RegisterScreen(navController: NavHostController) {
 
 @Composable
 private fun ScreenContent(modifier: Modifier, navController: NavController) {
+    var username by remember { mutableStateOf("") }
+    var fullName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmedPassword by remember { mutableStateOf("") }
 
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(60.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            OutlinedTextField(
+                value = username,
+                onValueChange = {
+                    username = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.username)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = fullName,
+                onValueChange = {
+                    fullName = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.fullname)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Words,
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.password)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = confirmedPassword,
+                onValueChange = {
+                    confirmedPassword = it
+                },
+                label = {
+                    Text(
+                        text = stringResource(R.string.confirmedpassword)
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+
+                    }
+                ),
+                shape = RoundedCornerShape(100.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(
+                modifier = Modifier.padding(10.dp)
+            )
+
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth().background(
+                    color = MaterialTheme.colorScheme.primary,
+                    shape = RoundedCornerShape(100.dp))
+                ,
+                border = BorderStroke(0.dp, Color.Transparent),
+                onClick = {
+
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.register),
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RegisterPreview() {
+    DigitalentTheme {
+        RegisterScreen(rememberNavController())
+    }
 }
