6c88362453d44eaed05ecf50cd84420ec9a77600
Rafi Hidayat <38679875+Virtue1040@users.noreply.github.com>
2025-06-24 10:31:50 +0700

    Menyesuaikan

--- app/src/main/java/com/rafihidayat/digitalent/util/ViewModelFactory.kt
+++ app/src/main/java/com/rafihidayat/digitalent/util/ViewModelFactory.kt
@@ -2,17 +2,14 @@ package com.rafihidayat.digitalent.util
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.rafihidayat.digitalent.database.ResepDao
-import com.rafihidayat.digitalent.ui.screen.DetailViewModel
-import com.rafi607062330092.assesment2.screen.MainViewModel
+import com.rafihidayat.digitalent.database.UserDao
+import com.rafihidayat.digitalent.ui.screen.MainViewModel
 
-class ViewModelFactory(private val dao: ResepDao) : ViewModelProvider.Factory {
+class ViewModelFactory(private val dao: UserDao) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(dao) as T
-        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
-            return DetailViewModel(dao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
