e793bc75186580b2cf96e5a895f0a2076a97934a
Rafi Hidayat <38679875+Virtue1040@users.noreply.github.com>
2025-06-24 12:00:02 +0700

    Menambahkan loginviewmodel saja

---
+++ app/src/main/java/com/rafihidayat/digitalent/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.rafihidayat.digitalent.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.rafihidayat.digitalent.database.UserDao
+
+class LoginViewModel(dao: UserDao) : ViewModel() {
+
+}
