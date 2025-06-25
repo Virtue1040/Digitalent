d56ca50b1469ae97e345d6ade62f3492b5f24be2
Rafi Hidayat <38679875+Virtue1040@users.noreply.github.com>
2025-06-25 10:10:52 +0700

    Menambahkan halaman2

--- app/src/main/java/com/rafihidayat/digitalent/navigation/Screen.kt
+++ app/src/main/java/com/rafihidayat/digitalent/navigation/Screen.kt
@@ -1,9 +1,19 @@
 package com.rafihidayat.digitalent.navigation
 
-const val KEY_ID_USER = "idUser"
+const val KEY_ID_MAHASISWA = "idMahasiswa"
 
 sealed class Screen(val route: String) {
     data object login: Screen("loginScreen")
     data object register: Screen("registerScreen")
     data object main: Screen("mainScreen")
+    data object list: Screen("listScreen")
+    data object information: Screen("infoScreen")
+
+    data object newData: Screen("formScreen")
+    data object editData: Screen("formScreen/{$KEY_ID_MAHASISWA}") {
+        fun withId(id: Long) = "formScreen/$id"
+    }
+    data object detailData: Screen("detailScreen/{$KEY_ID_MAHASISWA}") {
+        fun withId(id: Long)  = "detailScreen/$id"
+    }
 }
